54b03313b280d6c69a53fd3a4bf8f695e0448bc7
Daniel Sawyer <dansawyer1117@gmail.com>
2022-04-03 21:48:36 -0500

    initail commit

--- isPalindrome.js
+++ isPalindrome.js
@@ -1,1 +1,11 @@
-// Write your code below
+let ispalindrome = (word) => {
+    let palindrome = false 
+    for(i = 0; i < word.length; i++){
+        console.log(word[i]);
+        if(word[i] !== word[word.length - i - 1])
+        return palindrome
+   }
+   return palindrome = true
+}
+
+console.log(ispalindrome(`rubber`))
